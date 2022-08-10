66d620af876d763c4cceda191b8945f57a89ae61
jtrajano33 <justin.trajano33@gmail.com>
2022-08-10 21:56:48 +0800

    added sprite animation

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, memo } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Pedometer } from 'expo-sensors';
 import CircularProgress from 'react-native-circular-progress-indicator';
+import SpriteSheet from 'rn-sprite-sheet';
 
-export default function App() {
+function App() {
   const [available, setAvailable] = useState(""); //checks if pedometer is available for the device
   const [stepCount, setStepCount] = useState(0);
   const [calories, setCalories] = useState(0);
   const [distance, setDistance] = useState(0);
+
+  let dailyStep = 6000;
+  let isDailyDone = false;
+  let sprite = null;
 
   const fetchPedometerAvailability = async () => {
     let isPedometerAvailable = await Pedometer.isAvailableAsync();
@@ -18,21 +23,24 @@ export default function App() {
     const subscription = Pedometer.watchStepCount(res => {
       setStepCount(res.steps);
 
-      let distanceCovered = res.steps/1408;
+      let distanceCovered = res.steps / 1408;
       let cal = distanceCovered * 60
 
       setDistance(distanceCovered.toFixed(3))
       setCalories(cal.toFixed(3))
+
     })
+
+    fetchPedometerAvailability();
   }
 
   useEffect(() => {
     subscribePedometer();
-    fetchPedometerAvailability();
   }, [])
 
-  let dailyStep = 6000;
-  let isDailyDone = false;
+  useEffect(() => {
+    sprite.play({ type: 'walk', fps: 40 })
+  }, [stepCount])
 
   return (
     <View style={styles.container}>
@@ -45,8 +53,9 @@ export default function App() {
           value={stepCount}
           maxValue={6000}
           radius={100}
+          duration={250}
           inActiveStrokeColor={'#2ecc71'}
-          inActiveStrokeOpacity={0.2}
+          inActiveStrokeOpacity={0.3}
           title={`${stepCount} / ${dailyStep}`}
           titleColor={'white'}
           titleStyle={{ fontWeight: 'bold' }}
@@ -69,6 +78,19 @@ export default function App() {
           Distance Covered: {distance} Km
         </Text>
       </View>
+
+      <View style={{flex:1}}>
+        <SpriteSheet
+          ref={ref => (sprite = ref)}
+          source={require('./assets/spritesheets/spider_walking.png')}
+          columns={6}
+          rows={6}
+          imageStyle={{ marginTop: 5 }}
+          animations={{
+            walk: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35],
+          }}
+        />
+      </View>
     </View>
   );
 }
@@ -90,3 +112,6 @@ const styles = StyleSheet.create({
     marginBottom: 10
   }
 });
+
+
+export default memo(App);
